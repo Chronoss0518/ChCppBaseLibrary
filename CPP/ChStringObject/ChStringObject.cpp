1445a0fc1a167768a2cbfcfd2b6bf59bdc395fb0
Chronoss0518 <sinyusuke5000@gmail.com>
2025-11-19 17:58:55 +0900

    ・Addの追加実装 ・継承先のオブジェクト自らがSetなどを行えるように修正

--- CPP/ChStringObject/ChStringObject.cpp
+++ CPP/ChStringObject/ChStringObject.cpp
@@ -3,16 +3,15 @@
 
 #include"ChStringObject.h"
 
-#define METHOD_LIST(_charaType,_stringObjectType)\
+#define BASE_METHOD_LIST(_charaType,_stringObjectType)\
 void StringObjectBase::Set(StringObjectBase* _obj, const std::basic_string<_charaType>& _str){\
 	auto&& cast = dynamic_cast<_stringObjectType*>(_obj);\
 	if (ChPtr::NullCheck(cast))return;\
 	cast->str = _str;}\
-void StringObjectBase::Get(StringObjectBase* _obj, std::basic_string<_charaType>& _str){\
-	_str = ChStd::GetZeroChara<_charaType>();\
+template<> std::basic_string<_charaType> StringObjectBase::Get(StringObjectBase* _obj){\
 	auto&& cast = dynamic_cast<_stringObjectType*>(_obj);\
-	if (ChPtr::NullCheck(cast))return;\
-	_str = cast->str;}\
+	if (ChPtr::NullCheck(cast))return ChStd::GetZeroChara<_charaType>();\
+	return cast->str;}\
 bool StringObjectBase::Is(StringObjectBase* _obj, const std::basic_string<_charaType>& _str){\
 	auto&& cast = dynamic_cast<_stringObjectType*>(_obj);\
 	if (ChPtr::NullCheck(cast))return false;\
@@ -26,11 +25,71 @@ size_t StringObjectBase::Find(StringObjectBase* _obj, const std::basic_string<_c
 	if (ChPtr::NullCheck(cast))return std::basic_string<_charaType>::npos;\
 	return cast->str.find(_str, 0);}
 
+#define CHILD_METHOD_LIST(_charaType,_stringObjectType)\
+void _stringObjectType::Set(StringObjectBase* _obj){\
+	auto&& cast = dynamic_cast<_stringObjectType*>(_obj);\
+	if (ChPtr::NullCheck(cast))return;\
+	str = cast->str;}\
+bool _stringObjectType::Is(StringObjectBase* _obj){\
+	auto&& cast = dynamic_cast<_stringObjectType*>(_obj);\
+	if (ChPtr::NullCheck(cast))return false;\
+	return str == cast->str;}\
+bool _stringObjectType::IsConstain(StringObjectBase* _obj){\
+	auto&& cast = dynamic_cast<_stringObjectType*>(_obj);\
+	if (ChPtr::NullCheck(cast))return false;\
+	return str.find(cast->str) >= 0;}\
+void _stringObjectType::Add(StringObjectBase* _obj){\
+	auto&& cast = dynamic_cast<_stringObjectType*>(_obj);\
+	if (ChPtr::NullCheck(cast))return;\
+	str = str + cast->str;}\
+size_t _stringObjectType::Find(StringObjectBase* _obj){\
+	auto&& cast = dynamic_cast<_stringObjectType*>(_obj);\
+	if (ChPtr::NullCheck(cast))return std::basic_string<_charaType>::npos;\
+	return str.find(cast->str);}\
+
 
 using namespace ChCpp;
 
-METHOD_LIST(char, CharStringObject)
-METHOD_LIST(wchar_t, WCharStringObject)
-METHOD_LIST(char16_t, U16CharStringObject)
-METHOD_LIST(char32_t, U32CharStringObject)
-METHOD_LIST(char8_t, U8CharStringObject)
+
+void StringObjectBase::Set(StringObjectBase* _in, StringObjectBase* _out)
+{
+	if (_in->type != _out->type)return;
+	_in->Set(_out);
+}
+
+bool StringObjectBase::Is(StringObjectBase* _in, StringObjectBase* _out)
+{
+	if (_in->type != _out->type)return false;
+	return _in->Is(_out);
+}
+
+bool StringObjectBase::IsConstain(StringObjectBase* _in, StringObjectBase* _out)
+{
+	if (_in->type != _out->type)return false;
+	return _in->IsConstain(_out);
+}
+
+void StringObjectBase::Add(StringObjectBase* _base, StringObjectBase* _target)
+{
+	if (_base->type != _target->type)return;
+	_base->Add(_target);
+}
+
+size_t StringObjectBase::Find(StringObjectBase* _base, StringObjectBase* _target)
+{
+	if (_base->type != _target->type)return;
+	_base->Set(_target);
+}
+
+BASE_METHOD_LIST(char, CharStringObject)
+BASE_METHOD_LIST(wchar_t, WCharStringObject)
+BASE_METHOD_LIST(char16_t, U16CharStringObject)
+BASE_METHOD_LIST(char32_t, U32CharStringObject)
+BASE_METHOD_LIST(char8_t, U8CharStringObject)
+
+CHILD_METHOD_LIST(char, CharStringObject)
+CHILD_METHOD_LIST(wchar_t, WCharStringObject)
+CHILD_METHOD_LIST(char16_t, U16CharStringObject)
+CHILD_METHOD_LIST(char32_t, U32CharStringObject)
+CHILD_METHOD_LIST(char8_t, U8CharStringObject)
+
--- CPP/ChStringObject/ChStringObject.h
+++ CPP/ChStringObject/ChStringObject.h
@@ -11,6 +11,19 @@ namespace ChCpp
 	{
 	public:
 
+		enum class Type : unsigned char
+		{
+			Char,
+			WChar,
+			U16Char,
+			U32Char,
+#ifdef CPP20
+			U8Char,
+#endif
+		};
+
+	public:
+
 		virtual ~StringObjectBase(){}
 
 	public:
@@ -24,16 +37,14 @@ namespace ChCpp
 		static void Set(StringObjectBase* _obj, const std::basic_string<char8_t>& _str);
 #endif
 
+		static void Set(StringObjectBase* _in, StringObjectBase* _out);
+
+		virtual void Set(StringObjectBase* _obj) = 0;
+
 	public:
 
-		static void Get(StringObjectBase* _obj, std::basic_string<char>& _str);
-		static void Get(StringObjectBase* _obj, std::basic_string<wchar_t>& _str);
-		static void Get(StringObjectBase* _obj, std::basic_string<char16_t>& _str);
-		static void Get(StringObjectBase* _obj, std::basic_string<char32_t>& _str);
-
-#ifdef CPP20
-		static void Get(StringObjectBase* _obj, std::basic_string<char8_t>& _str);
-#endif
+		template<typename CharaType>
+		static std::basic_string<CharaType> Get(StringObjectBase* _obj);
 
 	public:
 
@@ -46,6 +57,11 @@ namespace ChCpp
 		static bool Is(StringObjectBase* _obj, const std::basic_string<char8_t>& _str);
 #endif
 
+		static bool Is(StringObjectBase* _obj, StringObjectBase* _str);
+
+		virtual bool Is(StringObjectBase* _obj) = 0;
+
+
 		static bool IsConstain(StringObjectBase* _obj, const std::basic_string<char>& _str);
 		static bool IsConstain(StringObjectBase* _obj, const std::basic_string<wchar_t>& _str);
 		static bool IsConstain(StringObjectBase* _obj, const std::basic_string<char16_t>& _str);
@@ -55,6 +71,24 @@ namespace ChCpp
 		static bool IsConstain(StringObjectBase* _obj, const std::basic_string<char8_t>& _str);
 #endif
 
+		static bool IsConstain(StringObjectBase* _obj, StringObjectBase* _str);
+
+		virtual bool IsConstain(StringObjectBase* _obj) = 0;
+
+	public:
+
+		static void Add(StringObjectBase* _obj, std::basic_string<char>& _str);
+		static void Add(StringObjectBase* _obj, std::basic_string<wchar_t>& _str);
+		static void Add(StringObjectBase* _obj, std::basic_string<char16_t>& _str);
+		static void Add(StringObjectBase* _obj, std::basic_string<char32_t>& _str);
+
+#ifdef CPP20
+		static void Add(StringObjectBase* _obj, std::basic_string<char8_t>& _str);
+#endif
+
+		static void Add(StringObjectBase* _base, StringObjectBase* _target);
+
+		virtual void Add(StringObjectBase* _obj) = 0;
 
 	public:
 
@@ -67,28 +101,104 @@ namespace ChCpp
 		static size_t Find(StringObjectBase* _obj, const std::basic_string<char8_t>& _str);
 #endif
 
+		static size_t Find(StringObjectBase* _base, StringObjectBase* _target);
+
+		virtual size_t Find(StringObjectBase* _obj) = 0;
+
+	protected:
+
+		Type type = Type::Char;
+
 	};
 
 	class CharStringObject :public StringObjectBase
 	{
+	public:
+
+		CharStringObject() { type = Type::Char; }
+		
+	public:
+
+		void Set(StringObjectBase* _obj)override;
+
+		bool Is(StringObjectBase* _obj)override;
+
+		bool IsConstain(StringObjectBase* _obj)override;
+
+		void Add(StringObjectBase* _obj)override;
+
+		size_t Find(StringObjectBase* _obj)override;
+
+	private:
 		friend StringObjectBase;
 		std::basic_string<char> str = ChStd::GetZeroChara<char>();
 	};
 
 	class WCharStringObject :public StringObjectBase
 	{
+	public:
+
+		WCharStringObject() { type = Type::WChar; }
+
+	public:
+
+		void Set(StringObjectBase* _obj)override;
+
+		bool Is(StringObjectBase* _obj)override;
+
+		bool IsConstain(StringObjectBase* _obj)override;
+
+		void Add(StringObjectBase* _obj)override;
+
+		size_t Find(StringObjectBase* _obj)override;
+
+	private:
 		friend StringObjectBase;
 		std::basic_string<wchar_t> str = ChStd::GetZeroChara<wchar_t>();
 	};
 
 	class U16CharStringObject :public StringObjectBase
 	{
+	public:
+
+		U16CharStringObject() { type = Type::U16Char; }
+
+	public:
+
+		void Set(StringObjectBase* _obj)override;
+
+		bool Is(StringObjectBase* _obj)override;
+
+		bool IsConstain(StringObjectBase* _obj)override;
+
+		void Add(StringObjectBase* _obj)override;
+
+		size_t Find(StringObjectBase* _obj)override;
+
+	private:
 		friend StringObjectBase;
 		std::basic_string<char16_t> str = ChStd::GetZeroChara<char16_t>();
 	};
 
 	class U32CharStringObject :public StringObjectBase
 	{
+	public:
+
+		U32CharStringObject() { type = Type::U32Char; }
+
+	public:
+
+		void Set(StringObjectBase* _obj)override;
+
+		bool Is(StringObjectBase* _obj)override;
+
+		bool IsConstain(StringObjectBase* _obj)override;
+
+		void Add(StringObjectBase* _obj)override;
+
+		size_t Find(StringObjectBase* _obj)override;
+
+	private:
 		friend StringObjectBase;
 		std::basic_string<char32_t> str = ChStd::GetZeroChara<char32_t>();
 	};
@@ -96,6 +206,23 @@ namespace ChCpp
 #ifdef CPP20
 	class U8CharStringObject :public StringObjectBase
 	{
+	public:
+
+		U8CharStringObject() { type = Type::U8Char; }
+
+	public:
+
+		void Set(StringObjectBase* _obj)override;
+
+		bool Is(StringObjectBase* _obj)override;
+
+		bool IsConstain(StringObjectBase* _obj)override;
+
+		void Add(StringObjectBase* _obj)override;
+
+		size_t Find(StringObjectBase* _obj)override;
+
+	private:
 		friend StringObjectBase;
 		std::basic_string<char8_t> str = ChStd::GetZeroChara<char8_t>();
 	};
